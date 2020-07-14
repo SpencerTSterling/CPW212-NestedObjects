a9b6342ae91e88dc2861e6b77fe5438057a1ff17
Spencer Sterling <spencer.sterling2001@gmail.com>
2020-07-13 19:29:27 -0700

    Added test code in main - functionality for Student, Instructor, and Course classes

--- NestedObjects/Program.cs
+++ NestedObjects/Program.cs
@@ -10,6 +10,66 @@ namespace NestedObjects
     {
         static void Main(string[] args)
         {
+            //create instructor
+            Instructor currProf = new Instructor()
+            {
+                FullName = "Homer Simpson",
+                Email = "Homer.Simpson@cptc.edu"
+            };
+
+            //create some students
+            Student stu1 = new Student()
+            {
+                FirstName = "Jim",
+                LastName = "Halpert"
+            };
+
+            Student stu2 = new Student()
+            {
+                FirstName = "Creed",
+                LastName = "Bratton"
+            };
+
+            Student stu3 = new Student()
+            {
+                FirstName = "Kelly",
+                LastName = "Kapur"
+            };
+
+            //create a course
+            Course testCourse = new Course();
+            testCourse.Title = "CPW212 - Advanced .NET Programming";
+            testCourse.Description = "Learn advanced .NET programming - " +
+                "writing classes, working with indexers, overloading operators, and other advanced object-oriented concepts";
+            testCourse.Credits = 5;
+
+                //give course the current professor
+            testCourse.CourseInstructor = currProf;
+
+                //give course a roster of students
+            testCourse.Roster = new List<Student>();
+
+            testCourse.Roster.Add(stu1);
+            testCourse.Roster.Add(stu2);
+            testCourse.Roster.Add(stu3);
+
+
+            // Show data
+
+            Console.WriteLine(testCourse.Title);
+            Console.WriteLine("is taught by " + testCourse.CourseInstructor.FullName);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Currently entrolled students:");
+            foreach(Student s in testCourse.Roster)
+            {
+                Console.WriteLine($"{s.FirstName} {s.LastName}");
+            }
+
+            // read key 
+            Console.ReadKey();
+
         }
     }
 }
